cd9f0affc945975c3c5887873a59c140e5566951
Buğrahan Gökçe <bugrahangokce2882@gmail.com>
2021-01-17 17:53:42 +0300

    17 Ocak Homework

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi : " + customer.Ad + " " +customer.Soyad);
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi : " + customer.Ad + " " + customer.Soyad);
+        }
+
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine("Müşteri Listelendi : " + "Id: " + customer.Id + " // " + " İsim: " + customer.Ad + " // " + " Soyisim: " + customer.Soyad + " //" + " Yaş: " + customer.Yas);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -10,6 +10,44 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Ad = "Buğrahan";
+            customer1.Soyad = "Gökçe";
+            customer1.Yas = 20;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Ad = "Bahtiyar";
+            customer2.Soyad = "Korkmaz";
+            customer2.Yas = 22;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Ad = "Ece";
+            customer3.Soyad = "Yılmaz";
+            customer3.Yas = 18;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müşteri Id'si : " + customer.Id);
+                Console.WriteLine("Müşteri İsmi : " + customer.Ad);
+                Console.WriteLine("Müşteri Soyismi : " + customer.Soyad);
+                Console.WriteLine("Müşteri Yaşı : " + customer.Yas);
+                Console.WriteLine("----------------");
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.CustomerAdd(customer1);
+            customerManager.CustomerDelete(customer2);
+            customerManager.CustomerList(customer1);
+            customerManager.CustomerList(customer2);
+            customerManager.CustomerList(customer3);
+
+            Console.ReadLine();
         }
     }
 }
